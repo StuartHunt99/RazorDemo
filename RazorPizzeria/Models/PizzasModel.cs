0217b457e9876e654a9d0c89304c1b709fcdb390
stuarthunt <stuarthuntcreative@gmail.com>
2022-10-15 17:07:48 -0400

    Added models to empty Razor pages

---
+++ RazorPizzeria/Models/PizzasModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace RazorPizzeria.Models
+{
+    public class PizzasModel
+    {
+        
+		public string ImageTile { get; set; }
+
+        public string PizzaName { get; set; }
+
+        public float BasePrice { get; set; } = 2;
+
+        public bool TomatoSauce { get; set; }
+
+        public bool Cheese { get; set; }
+
+        public bool Pepperoni { get; set; }
+
+        public bool Tuna { get; set; }
+
+        public bool Pineapple { get; set; }
+        
+        public bool Ham { get; set; }
+
+        public bool Beef { get; set; }
+
+        public float FinalPrice { get; set; }
+
+    }
+}
+
